b20a00044cd1b2267748f86cce2d5163cae3c648
Berglind Sigurðardóttir <berglinds14@ru.is>
2017-05-03 13:13:14 +0000

    entity

---
+++ CodeEditorApp/Models/Entity/Comment.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CodeEditorApp.Models.Entity
+{
+    public class Comment
+    {
+        public int ID { get; set; }
+        public string content { get; set; }
+        public string userID { get; set; }
+        public int projectID { get; set; }
+        public virtual AspNetUser user { get; set; }
+        public virtual Project project { get; set; }
+    }
+}
---
+++ CodeEditorApp/Models/Entity/Goal.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CodeEditorApp.Models.Entity
+{
+    public class Goal
+    {
+        public enum GoalType
+        {
+            Goal,
+            Objective
+        }
+
+        public class Goal
+        {
+            public int ID { get; set; }
+            public string name { get; set; }
+            public string description { get; set; }
+            public bool finished { get; set; }
+            public GoalType goalType { get; set; }
+            public string AspNetUserID { get; set; }
+            public int ProjectID { get; set; }
+            public virtual AspNetUser user { get; set; }
+            public virtual Project project { get; set; }
+        }
+    }
+}
